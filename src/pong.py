93a8b1686f162fa5be1c8d773448f65a19a73ec8
Haimonmon <beankor8@gmail.com>
2025-01-05 13:12:34 +0800

    Added all application pages in app instance attribute

--- src/pong.py
+++ src/pong.py
@@ -1,7 +1,5 @@
 import ui
-import retro
 import widgets as tkw
-
 
 class PongClassic:
     def __init__(self):
@@ -12,13 +10,22 @@ class PongClassic:
         self.widget_list = [] # * Planning to change this way of single paging soon :)
 
         self.main_menu = ui.MainMenuUI(self)
+        self.settings = ui.SettingsUI(self, previous_page = self.main_menu)
+        self.gamemodes = ui.GameModesUI(self, previous_page = self.main_menu)
+        self.playground_selection = ui.PlayGroundSelection(self, previous_page = self.gamemodes)
       
-        self.window.run(func=self.main_menu.render(), window_width=1000,window_height=580, window_title="Pong Classic ( Alpha )", resize_status=False)
-
+        self.window.run(
+            func = self.main_menu.render, 
+            window_width = 1000, 
+            window_height = 580, 
+            window_title = "Pong Classic ( Alpha )", 
+            resize_status = False, 
+            cursor_hidden = False
+        )
 
     def quit(self) -> None:
-        print(' Boinking as always :), Thank you. . . . <3')
         self.window.exit(self.widget_list)
+        print(' Boinking as always :), Thank you. . . . <3')
 
 if __name__ == "__main__":
     app = PongClassic()
