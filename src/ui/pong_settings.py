61ac4b8a2f73324a638268c06ea469453d5a7f93
Haimonmon <beankor8@gmail.com>
2025-01-05 13:09:31 +0800

    starting_pages adjustments

--- src/ui/pong_settings.py
+++ src/ui/pong_settings.py
@@ -1,4 +1,6 @@
 class SettingsUI:
+    starting_page = 'appearance'
+
     def __init__(self, app, previous_page = None):
         self.app = app
         self.previous_page = previous_page
@@ -13,6 +15,12 @@ class SettingsUI:
         self.controls = ControlSettings(self)
         self.sound = SoundSettings(self)
 
+        self.__sections = {
+            'appearance': self.appearance.render,
+            'controls': self.controls.render,
+            'sound': self.sound.render
+        }
+
 
     def render(self) -> None:
         self.app_window.set_title('Pong Classic | Settings ( Alpha )')
@@ -21,15 +29,12 @@ class SettingsUI:
         main_canvas = self.app_widget.create_canvas(width=1000, height=580, background="#1D313C", highlightthickness=0, pack={"fill": 'both', "expand": True})
         self.app_widget_list.append(main_canvas)
 
-        print('Main Canvas: ', main_canvas)
-
         self.inner_canvas = self.app_widget.create_canvas(parent_widget=main_canvas, width=955, height=534, background="#1D313C", highlightthickness=2, bd=0, relief='solid', highlightcolor='#FADAC1', place={"relx": 0.5, "rely": 0.5, "anchor": "center"})
         self.app_widget_list.append(self.inner_canvas)
 
         self.option_canvas = self.app_widget.create_canvas(parent_widget=main_canvas, width=498, height=403, background="#1D313C", highlightthickness=2, bd=0, relief='solid', highlightbackground='#FADAC1', place={"relx": 0.521, "rely": 0.55, "anchor": "center"})
 
-
-        self.controls.render()
+        self.__sections[SettingsUI.starting_page]()
 
 
     def handle_inputs(self) -> None:
@@ -42,6 +47,8 @@ class AppearanceSettings:
         
 
     def render(self) -> None:
+        SettingsUI.starting_page = 'appearance'
+
         self.settings.app_window.clear_canvas(self.settings.app_widget_list[1])
 
         self.settings.app_widget.create_canvas_button(master_canvas=self.settings.inner_canvas, text='Controls', width=159, height=42, x_coordinate=251,
@@ -62,6 +69,8 @@ class ControlSettings:
         self.settings = settings
 
     def render(self) -> None:
+        SettingsUI.starting_page = 'controls'
+
         self.settings.app_window.clear_canvas(self.settings.app_widget_list[1])
 
         self.settings.app_widget.create_canvas_button(master_canvas = self.settings.inner_canvas, text = 'Appearance', width = 180, height = 42, x_coordinate = 410, 
@@ -84,6 +93,8 @@ class SoundSettings:
 
 
     def render(self) -> None:
+        SettingsUI.starting_page = 'sound'
+
         self.settings.app_window.clear_canvas(self.settings.app_widget_list[1])
 
         self.settings.app_widget.create_canvas_button(master_canvas = self.settings.inner_canvas, text = 'Appearance', width = 180, height = 42, x_coordinate = 410, 
