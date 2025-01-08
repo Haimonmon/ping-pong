c06a5d3c20882bf61877aebe25631e2310c5dd0d
Haimonmon <beankor8@gmail.com>
2025-01-08 22:37:42 +0800

    Added minor helpers for apply thickness method

--- src/retrogine/elements/wall.py
+++ src/retrogine/elements/wall.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-from typing import Dict, Tuple, List
+from typing import Dict, Tuple, List, Callable
 
 class WallSegmentError(Exception):
     pass
@@ -14,8 +14,13 @@ class Wall:
     """
     Contains the walls around the playground or playfield 🧱
     """
-    def __init__(self, wall_coordinates_segments: List, playground: object, color: str = "white", thickness = 20):
-        self.wall_coordinates_segments = wall_coordinates_segments
+    def __init__(self, coordinates: List, playground: object, color: str = "white", thickness = 20):
+        '''
+        * wall comes up with start and end that each contain x and y coordinates
+        * Example : [(0,0), (0,1000)] 
+        * (x = 0,y = 0) is the starting point while (x = 0, y = 1000) is the ending point
+        '''
+        self.coordinates = coordinates
 
         self.playground = playground
 
@@ -30,136 +35,103 @@ class Wall:
         self.apply_thickness()
         self.render()
 
-
-    @property
-    def coordinates(self) -> dict:
-        return self.wall_coordinates_segments
-
-    
+ 
     def render(self):
         """
         Displays all of the wall on the given playground tkinter canvas
         """
-        for start_bottom, end_bottom, start_top, end_top in self.wall_coordinates_segments:
+        for start_bottom, end_bottom, start_top, end_top in self.coordinates:
             self.__platform.create_line(start_bottom[0], start_bottom[1], end_bottom[0], end_bottom[1], fill='blue')
             self.__platform.create_line(start_top[0], start_top[1], end_top[0], end_top[1], fill='blue')
 
             horizontal_wall = start_bottom[1] == end_bottom[1]
             vertical_wall = start_bottom[0] == end_bottom[0]
 
-
-
-            # if horizontal_wall:
-            #     self.__platform.create_rectangle(
-            #         start_bottom[0], start_bottom[1] + self.thickness, 
-            #         end_bottom[0], end_bottom[1], 
-            #         fill=self.color, outline="blue"
-            #     )
-
-            # elif vertical_wall:
-                
-            #     self.__platform.create_rectangle(
-            #         start_bottom[0] + self.thickness, start_bottom[1], 
-            #         end_bottom[0], end_bottom[1], 
-            #         fill=self.color, outline="blue"
-            #     )
-
-
     def check_walls(self) -> None:
         """
         Checks if wall coordinates given are valid
         """
-        for coordinates in self.wall_coordinates_segments:
-           
-            if (len(coordinates) != 2):
-                raise InvalidWallCoordinates(f'Needs a partner for ending and starting point. Invalid: {coordinates}')
+        for coordinate in self.coordinates:
+            if (len(coordinate) != 2):
+                raise InvalidWallCoordinates(f'Needs a partner for ending and starting point. Invalid: {coordinate}')
 
-            starting_wall = coordinates[0]
-            ending_wall = coordinates[1]
+            starting_wall = coordinate[0]
+            ending_wall = coordinate[1]
 
             if self.is_wall_exceeds(starting_wall) or self.is_wall_exceeds(ending_wall):
                 raise WallSegmentError(
-                        f"Invalid wall coordinates on side: {coordinates}. "
+                        f"Invalid wall coordinates on side: {coordinate}. "
                         f"Coordinates exceed the bounds (width={self.__platform_width}, height={self.__platform_height})."
                     )
             
 
     def is_wall_exceeds(self, wall_num: int) -> None:
         """
-        checks if the given wall_num exceeds on its limit
+        checks if the given wall coordinates are valid to its platform dimensions
         """
         return any(coord > limit or coord < 0 for coord, limit in zip(wall_num, (self.__platform_width, self.__platform_height)))
 
     
     def apply_thickness(self) -> None:
         """
-        Applies thickness to the walls by adjusting coordinates for both horizontal and vertical walls.
-        """
+        Applies thickness to the walls by adjusting coordinates
 
+        forming a complete rectangular box (top, bottom, left, and right sides)
+        """
         thickened_walls = []
 
-        half_thickness = (self.thickness / 2)
+        half_thickness = self.thickness // 2
 
+        # * Apply padding on the platform to not too compressed, this also helps to make the wall not overlapped when rendering with wall thickness
         padding = self.playground.platform_padding
 
-        for start, end in self.wall_coordinates_segments:
+        for start, end in self.coordinates:
 
-            horizontal_wall = start[1] == end[1]
-            vertical_wall = start[0] == end[0]
-
-            if horizontal_wall:
-                thickened_walls.append(
-                    [
-                        ((start[0] - half_thickness) + padding, (start[1] - half_thickness) + padding), ((end[0] + half_thickness) + padding, (end[1] - half_thickness) + padding),
-                        ((start[0] - half_thickness) + padding, (start[1] + half_thickness) + padding), ((end[0] + half_thickness) + padding, (end[1] + half_thickness) + padding)
+            if start[1] == end[1]: # * Horizontal wall
+                thickened_walls.append([
+                        *self._create_horizontal_side(start, end, -half_thickness + padding, -half_thickness + padding, +half_thickness + padding, -half_thickness + padding),
+                        *self._create_horizontal_side(start, end, -half_thickness + padding, +half_thickness + padding, +half_thickness + padding, +half_thickness + padding)
                     ]
                 )
 
                 thickened_walls.append(
                     [
-                        ((start[0] - half_thickness) + padding, (start[1] - half_thickness) + padding), ((start[0] - half_thickness) + padding, (start[1] + half_thickness) + padding),
-                        ((end[0] + half_thickness) + padding, (end[1] - half_thickness) + padding), ((end[0] + half_thickness) + padding, (end[1] + half_thickness) + padding)
+                        *self._create_vertical_side(start, -half_thickness + padding, -half_thickness + padding, -half_thickness + padding, +half_thickness + padding),
+                        *self._create_vertical_side(end, +half_thickness + padding, -half_thickness + padding, +half_thickness + padding, +half_thickness + padding)
                     ]
                 )
 
-            elif vertical_wall:
+                continue
+
+            elif start[0] == end[0]: # * Vertical wall
                 thickened_walls.append(
                     [
-                        ((start[0] - half_thickness) + padding, (start[1] - half_thickness) + padding), ((start[0] + half_thickness) + padding, (start[1] - half_thickness) + padding),
-                        ((end[0] - half_thickness) + padding, (end[1] + half_thickness) + padding), ((end[0] + half_thickness) + padding, (end[1] + half_thickness) + padding)
+                        *self._create_vertical_side(start, -half_thickness + padding, -half_thickness + padding, +half_thickness + padding, -half_thickness + padding),
+                        *self._create_vertical_side(end, -half_thickness + padding, +half_thickness + padding, +half_thickness + padding, +half_thickness + padding)
                     ]
                 )
 
                 thickened_walls.append(
                     [
-                        # * Top Side
-                        ((start[0] - half_thickness) + padding, (start[1] - half_thickness) + padding), ((end[0] - half_thickness) + padding, (end[1] + half_thickness) + padding),
-                        # * Bottom Side
-                        ((start[0] + half_thickness) + padding, (start[1] - half_thickness) + padding), ((end[0] + half_thickness) + padding, (end[1] + half_thickness) + padding)
+                        *self._create_horizontal_side(start, end, -half_thickness + padding, -half_thickness + padding, -half_thickness + padding, +half_thickness + padding),
+                        *self._create_horizontal_side(start, end, +half_thickness + padding, -half_thickness + padding, +half_thickness + padding, +half_thickness + padding)
                     ]
                 )
 
-        self.wall_coordinates_segments = thickened_walls
+        # * change old coordinates to new ones
+        self.coordinates = thickened_walls
 
+    def _create_horizontal_side(self, start_point, end_point, adjust_x1, adjust_y1, adjust_x2, adjust_y2) -> None:
+        return [
+            ((start_point[0] + adjust_x1), (start_point[1] + adjust_y1)), 
+            ((end_point[0]+ adjust_x2), (end_point[1] + adjust_y2))
+        ]
+
+    def _create_vertical_side(self, point, adjust_x1, adjust_y1, adjust_x2, adjust_y2) -> None:
+        return [
+            ((point[0] + adjust_x1), (point[1] + adjust_y1)),
+            ((point[0] + adjust_x2), (point[1] + adjust_y2))
+        ]
+        
 if __name__ == "__main__":
     pass
-
-# if horizontal_wall:
-#                 thickened_walls.append(
-#                     {
-#                         'top': ((start[0] - half_thickness, start[1] - half_thickness), (end[0] + half_thickness, end[1] - half_thickness)),
-#                         'bottom': ((start[0] - half_thickness, start[1] + half_thickness), (end[0] + half_thickness, end[1] + half_thickness)),
-#                         'left': ((start[0] - half_thickness, start[1] - half_thickness), (start[0] - half_thickness, start[1] + half_thickness)),
-#                         'right': ((end[0] + half_thickness, end[1] - half_thickness), (end[0] + half_thickness, end[1] + half_thickness))
-#                     }
-#                 )
-
-#             elif vertical_wall:
-#                 thickened_walls.append(
-#                     {
-#                         'top': ((start[0] - half_thickness, start[1] - half_thickness), (end[0] - half_thickness, end[1] + half_thickness)),
-#                         'bottom': ((start[0] + half_thickness, start[1] - half_thickness), (end[0] + half_thickness, end[1] + half_thickness)),
-#                         'left': ((start[0] - half_thickness, start[1] - half_thickness), (start[0] + half_thickness, start[1] - half_thickness)),
-#                         'right': ((end[0] - half_thickness, end[1] + half_thickness), (end[0] + half_thickness, end[1] + half_thickness))
-#                     }
-#                 )
