e1f5adbd27db4edf43548485d456dac46f0315f3
Haimonmon <beankor8@gmail.com>
2025-01-10 23:28:48 +0800

    Added conditionals for platform responsiveness

--- src/retrogine/elements/platform.py
+++ src/retrogine/elements/platform.py
@@ -1,13 +1,18 @@
 import tkinter as tk
 
 class Platform:
-    def __init__(self, playground: object, width: int, height: int, padding: int = 100, color: str = 'black'):
-        self.width = width
-        self.height = height
+    def __init__(self, playground: object, width: int, height: int, padding: int = 100, color: str = 'black', responsive: bool = False, new_width: int = None, new_height: int = None):
+        self.width = width # * Original width
+        self.height = height # * Original Height
+        
+        self.new_width = new_width
+        self.new_height = new_height
 
         self.padding = padding // 2
 
         self.color = color
+
+        self.responsive = responsive
 
         self.playground = playground
 
@@ -15,9 +20,15 @@ class Platform:
 
         self.render()
 
+
     def render(self) -> None:
-        platform_width = self.width + self.padding * 2
-        platform_height = self.height + self.padding * 2
+        if not self.new_height or not self.new_width or not self.responsive:
+            platform_width = self.width + self.padding * 2
+            platform_height = self.height + self.padding * 2
+        elif self.new_height and self.new_width and self.responsive:
+            platform_width = self.new_width + self.padding * 2
+            platform_height = self.new_height + self.padding * 2
+
 
         platform = tk.Canvas(
              self.playground.master, 
