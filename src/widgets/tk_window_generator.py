ac80dae3859cf999d6e118d59d96ec6521d5a440
Haimonmon <beankor8@gmail.com>
2025-01-18 11:51:39 +0800

    Minor fix on clearing key bindings on seperate pages

--- src/widgets/tk_window_generator.py
+++ src/widgets/tk_window_generator.py
@@ -1,6 +1,7 @@
 import tkinter as tk
+from ctypes import windll
 
-from typing import Callable
+from typing import Callable, Literal
 from .tk_helpers import OpenImage
 
 from .tk_widget_generator import WidgetGenerator
@@ -48,6 +49,8 @@ class WindowGenerator:
         if cls.__instance is None:
             cls.__instance = super(WindowGenerator, cls).__new__(cls)
             cls.__instance.__root = tkr.Tk()
+            cls.__instance.__root.minimized = False
+            cls.__instance.__root.maximized = False
             cls.__instance.__root.title("Reused Tkinter OwO | Window")
         return cls.__instance
 
@@ -73,8 +76,83 @@ class WindowGenerator:
     @classmethod
     def set_size(cls,width:int, height:int) -> None:
         """ Sets the tkinter window size by customizing width and height. """
-        cls.__instance.__root.geometry(f"{width}x{height}")
+        cls.__instance.__root.geometry(f"{width}x{height}+100+100")
+
+
+    @classmethod
+    def customize_title_bar(cls, color:str = "white", window_title: Literal['owo', 'meow', "Tk"] = "Tk", window_title_text_size: int = 10, title_bar_height: int = 30):
+        root = cls.__instance.__root
+
+        title_bar = tk.Frame(root, bg = color, relief = 'raised', bd = 0, highlightthickness = 0 , height = title_bar_height)
+        title_bar.pack(fill = 'x')
+        title_bar.pack_propagate(False)
+
+        # close_button = tk.Button(title_bar, text='  ×  ', command = cls.__instance.__root.destroy() ,bg='#10121f',padx=2,pady=2,font=("calibri", 13),bd=0,fg='white',highlightthickness=0)
+        expand_button = tk.Button(title_bar, text=' 🗖 ', command = lambda: cls.__instance._maximize_me(expand_button), bg='#10121f',padx=2,pady=2,bd=0,fg='white',font=("calibri", 13),highlightthickness=0)
+        minimize_button = tk.Button(title_bar, text=' 🗕 ',command= lambda: cls.__instance._minimize_me(), bg='#10121f',padx=2,pady=2,bd=0,fg='white',font=("calibri", 13),highlightthickness=0)
+
+        title_bar_title = tk.Label(title_bar, text=window_title, bg = color,bd = 0, fg = 'black' ,font=("Helvetica", window_title_text_size), highlightthickness=0)
+
+        # close_button.pack(side=RIGHT,ipadx=7,ipady=1)
+        expand_button.pack(side='right',ipadx=7,ipady=1)
+        minimize_button.pack(side='right',ipadx=7,ipady=1)
+        title_bar_title.pack(side='left', padx=10)
+
+        cls.__instance.__root.bind("<Map>", cls.__instance._deminimize_me)
     
+
+    @classmethod
+    # * 💡 creditz to: https://github.com/Terranova-Python/Tkinter-Menu-Bar/blob/main/main.py#L59
+    def _set_appwindow(cls, mainWindow: tk.Tk) -> None:
+        GWL_EXSTYLE = -20
+        WS_EX_APPWINDOW = 0x00040000
+        WS_EX_TOOLWINDOW = 0x00000080
+        # Magic
+        hwnd = windll.user32.GetParent(mainWindow.winfo_id())
+        stylew = windll.user32.GetWindowLongW(hwnd, GWL_EXSTYLE)
+        stylew = stylew & ~WS_EX_TOOLWINDOW
+        stylew = stylew | WS_EX_APPWINDOW
+        res = windll.user32.SetWindowLongW(hwnd, GWL_EXSTYLE, stylew)
+
+        mainWindow.wm_withdraw()
+        mainWindow.after(10, lambda: mainWindow.wm_deiconify())
+    
+
+    @classmethod
+    # * 💡 creditz fix from: https://stackoverflow.com/questions/63217105/tkinter-overridedirect-minimizing-and-windows-task-bar-issues
+    def _minimize_me(cls) -> None:
+        # so you can't see the window when is minimized
+        cls.__instance.__root.state('withdrawn')
+        cls.__instance.__root.overrideredirect(False)
+        cls.__instance.__root.state('iconic')
+        cls.__instance.__root.minimized = True
+
+
+    @classmethod
+    # * 💡 creditz fix from: https://stackoverflow.com/questions/63217105/tkinter-overridedirect-minimizing-and-windows-task-bar-issues
+    def _deminimize_me(cls, event) -> None:
+        cls.__instance.__root.focus()
+        # so you can see the window when is not minimized
+        cls.__instance.__root.overrideredirect(True)
+        if cls.__instance.__root.minimized == True:
+            cls.__instance.__root.minimized = False
+
+
+    @classmethod
+    def _maximize_me(cls, expand_button: tk.Button) -> None:
+        if cls.__instance.__root.maximized == False:  # if the window was not maximized
+            cls.__instance.__root.normal_size = cls.__instance.__root.geometry()
+            expand_button.config(text=" 🗗 ")
+            cls.__instance.__root.geometry(f"{cls.__instance.__root.winfo_screenwidth()}x{
+                        cls.__instance.__root.winfo_screenheight()}+0+0")
+            cls.__instance.__root.maximized = not cls.__instance.__root.maximized
+            # now it's maximized
+
+        else:  # if the window was maximized
+            expand_button.config(text=" 🗖 ")
+            cls.__instance.__root.geometry(cls.__instance.__root.normal_size)
+            cls.__instance.__root.maximized = not cls.__instance.__root.maximized
+            # now it is not maximized
 
     @classmethod
     def set_resize(cls, status: bool) -> None:
@@ -114,6 +192,11 @@ class WindowGenerator:
         """ Hides the cursor for no reason :) """
         if cursor_hidden:
             cls.__instance.__root.config(cursor = 'none')
+
+
+    @classmethod
+    def disable_titlebar(cls) -> None:
+        cls.__instance.__root.overrideredirect(True)
 
 
     @classmethod
@@ -162,7 +245,9 @@ class WindowGenerator:
         if func:
             func() # * This is for pages that needed to showup first when the application open
 
-   
+        # if disable_title_bar:
+        #     cls.__instance.disable_titlebar()
+
         cls.__instance.set_cursor(cursor_hidden)
         cls.__instance.set_resize(resize_status)
         cls.__instance.set_size(window_width,window_height)
@@ -175,7 +260,6 @@ class WindowGenerator:
     @classmethod
     def root(cls):
         return cls.__instance.__root
-
 
 if __name__ == "__main__":
     pass
